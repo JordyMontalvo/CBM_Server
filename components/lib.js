111b2e954e37e0e8309b5f34c8545cb2e804ad85
Jordy Montalvo <jordymontalvo@MacBook-Air-de-Jordy.local>
2025-12-16 13:17:10 -0500

    fix carga afiliaciones fix cors

--- components/lib.js
+++ components/lib.js
@@ -29,7 +29,7 @@ class Lib {
     }
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Cache-Control, Pragma, Expires');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Max-Age', '86400');
 
